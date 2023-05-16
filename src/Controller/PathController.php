7272798cd8ba410218afc67fbb9705939980e76b
doroshin <doroshinilia777@gmail.com>
2023-05-16 17:47:53 +0300

    Added some CRUD code

--- src/Controller/PathController.php
+++ src/Controller/PathController.php
@@ -2,23 +2,78 @@
 
 namespace App\Controller;
 
+use App\Entity\Path;
+use App\Form\PathType;
+use App\Repository\PathRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PathController extends AbstractController
 {
     #[Route('/path', name: 'main_path')]
-    public function index(): Response
+    public function index(PathRepository $pathRepository): Response
     {
         return $this->render('path/index.html.twig', [
-            'controller_name' => 'PathController',
+            'pathways' => $pathRepository->findAll(),
+            #'controller_name' => 'PathController',
+        ]);
+    }
+
+    #[Route('/view_path', name: 'view_path')]
+    public function show(Path $path): Response
+    {
+        return $this->render('path/view_path.html.twig', [
+            'pathway' => $path,
         ]);
     }
 
     #[Route('/add_path', name: 'add_path')]
-    public function addPath(): Response
+    public function new(Request $request, PathRepository $pathRepository): Response
     {
-        return $this->render('path/add_path.html.twig', []);
+        $path = new Path();
+        $form = $this->createForm(PathType::class, $path);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $path->setCreatedAt(time());
+            $path->setUpdatedAt(null);
+            $pathRepository->save($path, true);
+
+            return $this->redirectToRoute('view_path', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('path/add_path.html.twig', [
+            'pathway' => $path,
+            'form' => $form,
+        ]);
+        #return $this->render('path/add_path.html.twig', []);
+    }
+
+    public function edit(Request $request, Path $path, pathRepository $pathRepository): Response
+    {
+        $form = $this->createForm(PathType::class, $path);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pathRepository->add($path, true);
+
+            return $this->redirectToRoute('view_path', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('path/edit_path.html.twig', [
+            'pathway' => $path,
+            'form' => $form,
+        ]);
+    }
+
+    public function delete(Request $request, Path $path, pathRepository $pathRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$path->getId(), $request->request->get('_token'))) {
+            $pathRepository->remove($path, true);
+        }
+
+        return $this->redirectToRoute('main_path', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Entity/Path.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PathRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PathRepository::class)]
+class Path
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $region = null;
+
+    #[ORM\Column]
+    private ?int $created_at = null;
+
+    #[ORM\Column]
+    private ?int $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
