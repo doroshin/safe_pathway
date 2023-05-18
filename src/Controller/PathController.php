34cccaca246e450da375f86ec3f8ed6cd811d681
doroshin <doroshinilia777@gmail.com>
2023-05-18 14:44:41 +0300

    CRUD working now

--- src/Controller/PathController.php
+++ src/Controller/PathController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PathController extends AbstractController
 {
-    #[Route('/path', name: 'main_path')]
+    #[Route('/path', name: 'main_pathway')]
     public function index(PathRepository $pathRepository): Response
     {
         return $this->render('path/index.html.twig', [
@@ -21,7 +21,7 @@ class PathController extends AbstractController
         ]);
     }
 
-    #[Route('/view_path', name: 'view_path')]
+    #[Route('view_path/{id}', name: 'view_pathway', methods: ['GET'])]
     public function show(Path $path): Response
     {
         return $this->render('path/view_path.html.twig', [
@@ -29,7 +29,7 @@ class PathController extends AbstractController
         ]);
     }
 
-    #[Route('/add_path', name: 'add_path')]
+    #[Route('path/add_path', name: 'add_pathway', methods: ['GET', 'POST'])]
     public function new(Request $request, PathRepository $pathRepository): Response
     {
         $path = new Path();
@@ -38,28 +38,29 @@ class PathController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $path->setCreatedAt(time());
-            $path->setUpdatedAt(null);
+            $path->setUpdatedAt(0);
             $pathRepository->save($path, true);
 
-            return $this->redirectToRoute('view_path', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('view_pathway', ['id' => $path->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('path/add_path.html.twig', [
             'pathway' => $path,
             'form' => $form,
         ]);
-        #return $this->render('path/add_path.html.twig', []);
     }
 
+    #[Route('/path/{id}/edit_path   ', name: 'edit_pathway', methods: ['GET', 'POST'])]
     public function edit(Request $request, Path $path, pathRepository $pathRepository): Response
     {
         $form = $this->createForm(PathType::class, $path);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $pathRepository->add($path, true);
+            $path->setUpdatedAt(time());
+            $pathRepository->save($path, true);
 
-            return $this->redirectToRoute('view_path', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('view_pathway', ['id' => $path->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('path/edit_path.html.twig', [
@@ -68,12 +69,13 @@ class PathController extends AbstractController
         ]);
     }
 
+    #[Route('/path/{id}   ', name: 'delete_pathway', methods: ['POST'])]
     public function delete(Request $request, Path $path, pathRepository $pathRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$path->getId(), $request->request->get('_token'))) {
             $pathRepository->remove($path, true);
         }
 
-        return $this->redirectToRoute('main_path', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('main_pathway', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Entity/Path.php
+++ src/Entity/Path.php
@@ -54,24 +54,24 @@ class Path
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?int
     {
         return $this->created_at;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    public function setCreatedAt(int $created_at): self
     {
         $this->created_at = $created_at;
 
         return $this;
     }
 
-    public function getUpdatedAt(): ?\DateTimeImmutable
+    public function getUpdatedAt(): ?int
     {
         return $this->updated_at;
     }
 
-    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    public function setUpdatedAt(int $updated_at): self
     {
         $this->updated_at = $updated_at;
 
